2fc360efdfe07eb4d68f6f2efb0e49bb7443d1da
William Clark <35467425+16wclark@users.noreply.github.com>
2019-04-11 11:41:39 -0400

    menu done

--- Friendly File Manager/menu.cpp
+++ Friendly File Manager/menu.cpp
@@ -1,24 +1,38 @@
-#include "Primary Feature 1.h";
-#include "Primary Feature 2.h";
-#include "Special Feature 1.h";
-#include "Special Feature 2.h";
-#include <iostream>
-#include <fstream>
-using namespace std;
+#include "menu.h"
+
 
 void menu() {
 	fstream sacrificeFile;
-
-
+	string fileName;
 	bool fileSelected = false;
-	cout << "Please select a file to proceed";
+	string choice;
+
+	cout << "Please select a file to proceed, with extension";
+	cin >> fileName; 
+	sacrificeFile.open(fileName);
+	fileSelected = true;
+
 
 	if (fileSelected) {
 		cout << "Main Menu";
-		cout << "    1. Read and display the file";
-		cout << "    2. Encrypt the file";
+		cout << "    1. Encrypt the file";
+		cout << "    2. Read and display the file";
 		cout << "    3. Special feature 1";
 		cout << "    4. Special feature 2";
+		cin >> choice; //can't do switch with a string I found out
+		if (choice == "1")
+			primaryFeature1(sacrificeFile);
+		else if (choice == "2")
+			primaryFeature2(sacrificeFile);
+		else if (choice == "3")
+			primaryFeature3(sacrificeFile);
+		else if (choice == "4")
+			primaryFeature4();
+		else
+			cout << "Invalid input detected.";
+
+
+		
 	}
 
 }
