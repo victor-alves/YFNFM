9ad25c60d482109fcf0d83ce1c07a7e12519acca
William Clark <35467425+16wclark@users.noreply.github.com>
2019-04-11 15:08:29 -0400

    Delete Primary Feature 1.cpp

--- Primary Feature 1.cpp
+++
@@ -1,8 +0,0 @@
-#include "Primary Feature 1.h";
-
-void primaryFeature1(string fileName) {
-	ofstream fileBreaker;
-	fileBreaker.open(fileName, ofstream::out | ofstream::trunc);
-	fileBreaker << randomString;
-	fileBreaker.close();
-}
