1d1839293c5b0b94535bf934f3f91bd123eeaed1
William Clark <35467425+16wclark@users.noreply.github.com>
2019-04-11 11:05:09 -0400

    working on menu, not done

---
+++ Friendly File Manager/menu.cpp
@@ -0,0 +1,24 @@
+#include "Primary Feature 1.h";
+#include "Primary Feature 2.h";
+#include "Special Feature 1.h";
+#include "Special Feature 2.h";
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void menu() {
+	fstream sacrificeFile;
+
+
+	bool fileSelected = false;
+	cout << "Please select a file to proceed";
+
+	if (fileSelected) {
+		cout << "Main Menu";
+		cout << "    1. Read and display the file";
+		cout << "    2. Encrypt the file";
+		cout << "    3. Special feature 1";
+		cout << "    4. Special feature 2";
+	}
+
+}
