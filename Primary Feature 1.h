9fd2e40b81a61986ba5c87591e24555897b592b1
William Clark <35467425+16wclark@users.noreply.github.com>
2019-04-11 15:08:35 -0400

    Delete Primary Feature 1.h

--- Primary Feature 1.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <fstream>
-using namespace std;
-
-const static string randomString = "63926333002981567222968699769317826165437687805605933939202151316157876928082643513466200690379896458283";
-
-void specialFeature2(string fileName) {};
