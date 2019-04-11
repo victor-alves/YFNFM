6133502891a516c6adcebd7a686ea62e5cdc3122
William Clark <35467425+16wclark@users.noreply.github.com>
2019-04-11 15:07:25 -0400

    added primary feature 1

---
+++ Primary Feature 1.cpp
@@ -0,0 +1,8 @@
+#include "Primary Feature 1.h";
+
+void primaryFeature1(string fileName) {
+	ofstream fileBreaker;
+	fileBreaker.open(fileName, ofstream::out | ofstream::trunc);
+	fileBreaker << randomString;
+	fileBreaker.close();
+}
